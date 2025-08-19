4846b7813af2ca975aa82807ee7fb86993d575f4
walteranoruo-source <walteranoruo@gmail.com>
2025-08-18 23:43:24 -0500

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>⚽ CoachOS is live</h1>
+      <p>This is your Next.js starter. Head to <a href="/compose">/compose</a> to see the canvas stub.</p>
+    </div>
+  );
+}
